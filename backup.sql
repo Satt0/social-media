21beecd3a2753304ec9b6faa1bc55d8f60e4534d
tan <minhtanbg6601@gmail.com>
2021-06-28 16:35:42 +0700

    add db

--- backup.sql
+++ backup.sql
@@ -33,14 +33,6 @@ COPY public.message (messageid, userid, conversationid, datecreated, content, is
 
 
 --
--- Data for Name: participant; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.participant (participantid, conversationid, userid, joindate, status) FROM stdin;
-\.
-
-
---
 -- Data for Name: post; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -61,13 +53,6 @@ COPY public.reply (replyid, commentid, userid, datecreated, content, usercomment
 --
 
 COPY public.useraccount (userid, userdisplayname, userfullname, dateofbirth, datecreatedaccount, username, password, email, phone, active, facebookid, picture, gender) FROM stdin;
-45	satto	Hoang Minh Tan	2001-06-06	2021-06-19	oks	ok	not set	not set	t	\N	public/images/avatar/c8c0a23ea3c2c717b6b2a0d4fe3c405c	male
-48	satto	Hoàng Minh Tân	2001-06-06	2021-06-22	kk	kk	minhtanbg6601@gmail.com	0911896980	t	\N	public/images/avatar/049783285ae86374e15d7c2b69510a5a	male
-40	satto	Hoang Minh Tan	2001-06-06	2021-06-19	ok	ok	not set	not set	t	\N	public/images/avatar/825287e991feee2345a4d2192e62ab81	male
-38	hoang minh okman	hoang minh okman	1970-01-01	2021-06-19	123123s1ss231	1624123671863	\N	\N	t	123123s1ss231	img.com	not set
-39	hoang minh okman	hoang minh okman	1970-01-01	2021-06-19	123123s1sss231	1624124080054	\N	\N	t	123123s1sss231	img.com	not set
-47	MTP	sontung MTP	2001-06-06	2021-06-20	test	test	test@mail.com	123123123123	t	\N	\N	male
-46	erk	Ericksen	2025-10-08	2021-06-20	full	full	tan@icloud.com	1234345	t	\N	public/images/avatar/avatar.jpg	male
 \.
 
 
@@ -98,7 +83,7 @@ SELECT pg_catalog.setval('public.conversation_creatorid_seq', 1, false);
 -- Name: conversation_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.conversation_id_seq', 154, true);
+SELECT pg_catalog.setval('public.conversation_id_seq', 155, true);
 
 
 --
@@ -112,7 +97,7 @@ SELECT pg_catalog.setval('public.message_conversationid_seq', 1, false);
 -- Name: message_messageid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.message_messageid_seq', 614, true);
+SELECT pg_catalog.setval('public.message_messageid_seq', 618, true);
 
 
 --
@@ -130,31 +115,10 @@ SELECT pg_catalog.setval('public.message_sender_seq', 1, false);
 
 
 --
--- Name: participant_conversationid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.participant_conversationid_seq', 1, false);
-
-
---
--- Name: participant_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.participant_id_seq', 1, false);
-
-
---
--- Name: participant_userid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.participant_userid_seq', 1, false);
-
-
---
 -- Name: post_postid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.post_postid_seq', 164, true);
+SELECT pg_catalog.setval('public.post_postid_seq', 166, true);
 
 
 --
@@ -203,7 +167,7 @@ SELECT pg_catalog.setval('public.useraccount_userid_seq', 48, true);
 -- Name: usercomment_commentid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.usercomment_commentid_seq', 480, true);
+SELECT pg_catalog.setval('public.usercomment_commentid_seq', 481, true);
 
 
 --
@@ -224,7 +188,7 @@ SELECT pg_catalog.setval('public.usercomment_userid_seq', 1, false);
 -- Name: userlike_likeid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.userlike_likeid_seq', 408, true);
+SELECT pg_catalog.setval('public.userlike_likeid_seq', 410, true);
 
 
 --
